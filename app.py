376767ebe6ad501af1e5c085a13e979b47d3a6bf
Oğuz Elçi <oguz.elci@gmail.com>
2023-02-07 18:11:32 +0300

    code un son satırı silindi ve app adı tekrar eski adına çevrildi

--- app.py
+++ app.py
@@ -383,5 +383,5 @@ class Personel(db.Model, UserMixin):
         return '<Personel %r>' % self.name
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
 
