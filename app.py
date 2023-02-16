d209886181936b91dce5fd16fcf948ff9a3f758b
Oğuz Elçi <oguz.elci@gmail.com>
2023-02-16 11:56:09 +0300

    app son satırı railway flask template i gibi değiştirildi

--- app.py
+++ app.py
@@ -384,5 +384,5 @@ class Personel(db.Model, UserMixin):
         return '<Personel %r>' % self.name
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000, debug=True)
+    app.run(debug=True, port=os.getenv("PORT", default=5000)
 
